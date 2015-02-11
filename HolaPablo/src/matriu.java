01defc470063811dee8025fe715aa628113f9503
pablo <pablomagub@gmail.com>
2015-02-11 13:31:18 +0100

    solucionado errores de random

--- HolaPablo/src/matriu.java
+++ HolaPablo/src/matriu.java
@@ -5,25 +5,32 @@ public class matriu {
     //private static String paraules[]={"IVAN","HA","FOTUT","EL","MAC"};
     private static char lletres[]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     public matriu(){
-    	matriua= new char[FILES][COLUMNES];
     	
     }
 	public static void main(String[] args) {
-		//inicializaMatriu();
+		matriua= new char[FILES][COLUMNES];	
+    	inicializaMatriu();
 		ompliMatriu();
 		mostraMatriu();
 	}
+	public static int generaNumeroAleatorio(int minimo, int maximo){
+        
+        int num=(int)Math.floor(Math.random()*(minimo-(maximo+1))+(maximo+1));
+        return num;
+    }
 	public static void inicializaMatriu(){
-		for(int i=0;i<FILES;i++){
-			for(int j=0;j<COLUMNES;j++){
+		for(int i=0;i<COLUMNES;i++){
+			for(int j=0;j<FILES;j++){
 				matriua[i][j]='*';
 			}
 		}
 	}
 	public static void ompliMatriu(){
-        for(int i=0;i<FILES;i++){
-            for(int j=0;j<COLUMNES;j++){
-                matriua[i][j]=lletres[(int) Math.random()*lletres.length-1];
+        for(int i=0;i<COLUMNES;i++){
+            for(int j=0;j<FILES;j++){
+            	int nume=generaNumeroAleatorio(0,lletres.length-1);
+            	
+                matriua[i][j]=lletres[nume];
            	}
         }
 	}
