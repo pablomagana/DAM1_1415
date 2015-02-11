eddc3bd78f6932b26435d56bbd01e941b6d563b8
pablo <pablomagub@gmail.com>
2015-02-11 12:35:52 +0100

    añadido random

--- HolaPablo/src/matriu.java
+++ HolaPablo/src/matriu.java
@@ -2,14 +2,15 @@ public class matriu {
 	private static char matriua[][];
     private static final int FILES=15;
     private static final int COLUMNES=15;
-    private static String paraules[]={"IVAN","HA","FOTUT","EL","MAC"};
-    private char lletres[]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    //private static String paraules[]={"IVAN","HA","FOTUT","EL","MAC"};
+    private static char lletres[]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     public matriu(){
     	matriua= new char[FILES][COLUMNES];
     	
     }
 	public static void main(String[] args) {
-		inicializaMatriu();
+		//inicializaMatriu();
+		ompliMatriu();
 		mostraMatriu();
 	}
 	public static void inicializaMatriu(){
@@ -20,10 +21,9 @@ public class matriu {
 		}
 	}
 	public static void ompliMatriu(){
-		char lletra=0;
         for(int i=0;i<FILES;i++){
             for(int j=0;j<COLUMNES;j++){
-                matriua[i][j]=lletra;
+                matriua[i][j]=lletres[(int) Math.random()*lletres.length-1];
            	}
         }
 	}
