a79945e570aa86b0df1c41b442ebb483278003ff
pablo <pablo@192.168.1.139>
2015-02-19 13:08:19 +0100

    p

---
+++ HolaPablo/src/JulioCesarConClave.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+
+public class JulioCesarConClave {
+
+	public static void main(String[] args) {
+		char base[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+		int clave=7;
+	}
+
+}
