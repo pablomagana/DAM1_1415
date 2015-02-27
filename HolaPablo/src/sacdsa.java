5f567a50629833943fa5b37a124e74297b8a7499
pablo <pablomagub@gmail.com>
2015-02-27 18:09:02 +0100

    v0.2

--- HolaPablo/src/sacdsa.java
+++ HolaPablo/src/sacdsa.java
@@ -1,12 +1,23 @@
+import javax.security.auth.callback.TextOutputCallback;
+import javax.swing.JOptionPane;
+
 
 public class sacdsa {
+static char letras[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z',' '};
+static int[] codigos=new int[letras.length];
+static int grupo=4;
+static int fila=7;
+static char letra;
+static String textoACodificar="";
+static String textoCodificado="";
 
 	public static void main(String[] args) {
-		char letras[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z',' '};
-		int[] codigos=new int[letras.length];
-		int grupo=4;
-		int fila=7;
+		codificador();
+		codificar(JOptionPane.showInputDialog(null,"escribe la frase que quieres codificar"));
+	}	
 		
+		
+	public static void codificador(){
 		int posicion=0;
 		for(int i=fila;i>0;i--){
 			for(int j=grupo;j>0;j--){
@@ -16,13 +27,14 @@ public class sacdsa {
 			
 		}
 		
-		for(int i=0;i<letras.length;i++){
-			System.out.print(" "+codigos[i]+" ");
-		}
-		System.out.println();
-		String textoACodificar="hola mundo";
-		String textoCodificado="";
-		char letra;
+		//for(int i=0;i<letras.length;i++){
+		//	System.out.print(" "+codigos[i]+" ");
+		//}
+		//System.out.println();
+	}
+	
+	public static void codificar(String aCodificar){
+		textoACodificar=aCodificar;
 		for(int i=0;i<textoACodificar.length();i++){
 			letra=textoACodificar.charAt(i);
 			for(int j=0;j<letras.length;j++){
