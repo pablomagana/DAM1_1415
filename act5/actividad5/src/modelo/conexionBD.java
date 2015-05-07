cef438b1e72c0f08213118bf9afd6fa53da23e8b
pablo <pablomagub@gmail.com>
2015-05-07 03:01:18 +0200

    conexion bd finalizada
    Signed-off-by: pablo <pablomagub@gmail.com>

--- act5/actividad5/src/modelo/conexionBD.java
+++ act5/actividad5/src/modelo/conexionBD.java
@@ -1,17 +1,20 @@
 package modelo;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class conexionBD {
 //datos de la conexion
 	static final String mysql_controler="com.mysql.jdbc.Driver";
 
 //datos de la base de datos
-	private String url;
+	private String host;
 	private String basedatos;
 	private String user;
 	private String password;
 	
-	public conexionBD(String url,String basedatos,String user,String password){
-		this.url=url;
+	public conexionBD(String host,String basedatos,String user,String password){
+		this.host=host;
 		this.basedatos=basedatos;
 		this.user=user;
 		this.password=password;
@@ -20,12 +23,15 @@ public class conexionBD {
 		//carga controlador para la conexion else return false
 		try{
 			Class.forName(mysql_controler);
-			return true;
-		}
-		catch(ClassNotFoundException e){
+			//conectar a bd
+			DriverManager.getConnection("jdbc:mysql://"+this.host+"/"+this.basedatos,this.user,this.password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}catch(ClassNotFoundException e){
 			e.printStackTrace();
 			return false;
 		}
+		return true;
 	}
 
 }
